2b1b1adfc4715b9eaee80d9e77ec21a63f7aee4c
san-san <boisannguyen5@gmail.com>
2021-11-23 22:43:57 +0700

    continute

--- webfrontend/script.js
+++ webfrontend/script.js
@@ -9,3 +9,15 @@ window.addEventListener("scroll", function(){
 	if(x>80){toP.classList.add("active")}
 		else{toP.classList.remove("active")}
 })
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click", function(x){
+	if(x.target.classList.contains("menu-button")){
+		const Target = x.target.getAttribute("data-title");
+		menuTitle.querySelector(".active").classList.remove("active");
+		x.target.classList.add("active");
+
+		const menuItem = document.querySelector(".menu");
+		menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+		menuItem.querySelector(Target).classList.add("active")
+	}
+})
